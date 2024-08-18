badaceb19a2b8bc65f93bbe2058550f5047d8c5d
divyansh <divyanshc494@gmail.com>
2024-08-18 14:57:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Tic from './components/Tic/Tic';
+
+
+
+
+
+function App() {
+  return (
+<div>
+  <Tic/>
+</div>
+  
+  );
+}
+
+export default App;
